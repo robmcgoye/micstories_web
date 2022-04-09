551179887526500c43c09d343582c133d235d94b
Rob McGoye <rob@mcgoye.com>
2022-04-09 19:45:09 -0400

    intial commit

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,38 @@
+$(document).on('turbolinks:load', function() {
+  let isCollapsed = false;
+
+  set_chat_height();
+  collapse_sidebar();
+  
+  function collapse_sidebar(){
+    $("#sidebar_button_icon").removeClass("bi-file-excel");
+    $("#sidebar_button_icon").addClass("bi-list");
+    $( "#sidebar" ).hide();
+    isCollapsed = true;
+  }
+
+  function expand_sidebar(){
+    $("#sidebar_button_icon").removeClass("bi-list");
+    $("#sidebar_button_icon").addClass("bi-file-excel");
+    $( "#sidebar" ).show();
+    isCollapsed = false;
+  }
+
+  $("#sidebarCollapse").click(function(){
+    if (!isCollapsed) {
+      collapse_sidebar();
+    } else {
+      expand_sidebar();
+    }
+  });
+
+  function set_chat_height(){
+    if ($("#chat").height() > $("#story").height()) {
+      $('#chat').css('height', $("#story").height());
+    }
+  }
+
+  $(window).resize(function(){
+    set_chat_height();
+  });
+});
