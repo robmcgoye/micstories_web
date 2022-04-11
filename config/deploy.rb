1294d5dcccad4d4862707e5f1923f9ae2716e431
Rob McGoye <rob@mcgoye.com>
2022-04-11 12:07:07 -0400

    setting up deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.0"
 
 set :application, "micstories_web"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:robmcgoye/micstories_web.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
